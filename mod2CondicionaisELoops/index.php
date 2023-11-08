58584ef3e031a288a39225a265357cb24e1be6ce
phpablo <pablohenrique120@gmail.com>
2023-11-08 14:12:02 -0300

    :tada: Iniciando o projeto

---
+++ mod2CondicionaisELoops/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$ingredientes = [
+  'açucar',
+  'farinha de trigo',
+  'ovo',
+  'leite',
+  'fermento em pó'
+];
+
+foreach ($ingredientes as $ingredientes) {
+  echo 'Item: ' . $ingredientes . '<br/>';
+};
