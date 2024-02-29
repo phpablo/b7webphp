15df237e1846c39a116198fb97f5fd99d270595d
phpablo <pablohenrique120@gmail.com>
2024-02-29 11:53:31 -0300

    :pencil: Add file about Encapsulamento
    - Add arquivos da aula sobre Encapsulamento de POO

---
+++ mod6POO/aula5Encapsulamento/index.php
@@ -0,0 +1,34 @@
+<?php
+// classe com atributos de um post 
+class Post { // Aqui é uma classe
+  public int $likes = 0; // int = agora só recebe tipo INT
+  public array $comments = []; // ARRAY = agora só recebe tipo ARRAY
+  public string $author; // STRING = agora só recebe tipo STRING, mas se digitar '10', ele recebe, mas como string 
+
+  public function __construct($qtdLikes = 0) {
+    $this->likes = $qtdLikes;
+  }
+
+  public function aumentarLikes() {
+    $this->likes++;
+  }
+
+  public function setAuthor($n) {
+    $this->author = $n;
+  }
+
+  public function getAuthor() {
+    return $this->author;
+  }
+}
+
+// cria um objeto da classe Post 
+$post1 = new Post(); // Isso é um objeto
+$post1->setAuthor('Jão das Neves');
+// cria um objeto da classe Post 
+// $post2 = new Post(); // Isso é um objeto
+
+
+// Escreve na tela o conteúdo do atritubo likes setados anteriormente
+echo 'Post 1 tem ' . $post1->likes . ' likes. E autor é ' . $post1->getAuthor() . '<br><hr>';
+// echo 'Post 2 tem ' . $post2->likes . ' likes.<br><hr>';
